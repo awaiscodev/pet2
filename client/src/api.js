91fef69ba713127ebcc6e6861d7bf96b55c5ee35
awaiscodev <awaiscodev.pk@gmail.com>
2026-05-30 19:38:21 +0500

    Update pet2 backend API URL

--- client/src/api.js
+++ client/src/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5000/api"
-      : "https://pet-insurance-virid.vercel.app/api",
+      : "https://YOUR-NEW-PET2-BACKEND.vercel.app/api",
 });
 
 export default api;
